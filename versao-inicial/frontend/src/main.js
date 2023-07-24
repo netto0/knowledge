f6ef426db873b36b56b12160b8b6ca32456f36f7
Netto <orlandiomnetto@gmail.com>
2023-07-24 20:56:31 -0300

    finish

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -4,13 +4,12 @@ import Vue from "vue";
 import App from "./App";
 import "./config/bootstrap";
 import './config/msgs'
+import './config/axios'
+import './config/mq'
 import store from "./config/store";
 import router from "./config/router";
 
 Vue.config.productionTip = false;
-
-require("axios").defaults.headers.common["Authorization"] =
-  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ik5ldHRvIiwiZW1haWwiOiJvcmxhbmRpb21uZXR0b0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjg5NzkwNTE0LCJleHAiOjE2OTAwNDk3MTR9.iTSf2SJhzOMiG5Wp8uUwP5Mm7QsluhdIhy6H0bqexBA";
 
 new Vue({
   store,
